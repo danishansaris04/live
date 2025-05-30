37870bc6ab854fd14f2242d158a66341b6a1d380
danishansaris04 <danishansariserver@gmail.com>
2025-05-30 15:12:10 +0530

    Create danish.js

---
+++ danish.js
@@ -0,0 +1,5 @@
+const videoList = [
+      { src: "https://dl.dropboxusercontent.com/scl/fi/85g1ethsruqn72we09zlq/A-Call-From-the-Teacher-3979351.mkv?rlkey=ecv40s2i7z3alb887lsqq9l31&st=3m7tm78c&raw=1" },
+      { src: "https://dl.dropboxusercontent.com/scl/fi/7inl2vx6chug056hjzsao/A-Cloud-Ride-3979294.mkv?rlkey=jz5ekz7jpv47aksivcxh06sd4&st=ah2a9cxx&raw=1" },
+      { src: "https://dl.dropboxusercontent.com/scl/fi/jyj2grjbkx89la2p8skca/A-Courageous-Proposal-3979196.mkv?rlkey=x0en0ktlq5ea42yi0uubaoy79&st=zqlhqkkg&raw=1" }
+    ];
